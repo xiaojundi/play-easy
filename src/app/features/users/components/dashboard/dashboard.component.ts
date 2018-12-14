aa367f73859152b4b7400574187f1f96b2b62a33
xiaojundi <dixiaojunfrank@gmail.com>
2018-12-14 12:35:22 -0800

    update

--- src/app/features/users/components/dashboard/dashboard.component.ts
+++ src/app/features/users/components/dashboard/dashboard.component.ts
@@ -9,10 +9,11 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(){
-
+    
   }
   ngAfterViewInit() {
     //We loading the player script on after view is loaded
     System.import('./videoConfig.js');
   }
+
 }
